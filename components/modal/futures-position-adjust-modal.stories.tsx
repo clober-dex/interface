d84817f790eac1e03b88891459461e53654cf961
graykode <nlkey2022@gmail.com>
2025-05-26 12:22:03 +0900

    refactor: update priceFeedId to a placeholder value for consistency across story files

--- components/modal/futures-position-adjust-modal.stories.tsx
+++ components/modal/futures-position-adjust-modal.stories.tsx
@@ -24,16 +24,14 @@ export const Default: Story = {
         name: 'AAPL',
         symbol: 'AAPL',
         icon: 'https://www.pyth.network/_next/image?url=%2Ficons%2Fprice-feed-icons%2Flight%2Fequity-us-aapl-usd.inline.svg&w=1920&q=75',
-        priceFeedId:
-          '0xafcc9a5bb5eefd55e12b6f0b4c8e6bccf72b785134ee232a5d175afd082e8832',
+        priceFeedId: '0x',
       },
       collateral: {
         address: '0x43D614B1bA4bA469fAEAa4557AEAFdec039b8795',
         name: 'USD Coin',
         symbol: 'USDC',
         decimals: 6,
-        priceFeedId:
-          '0xafcc9a5bb5eefd55e12b6f0b4c8e6bccf72b785134ee232a5d175afd082e8832',
+        priceFeedId: '0x',
       },
       expiration: 1635724800,
       maxLTV: 700000n,
