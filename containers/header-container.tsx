787c6910ef2e55692667eb0cae281d43b06333c7
graykode <nlkey2022@gmail.com>
2025-05-29 16:06:41 +0900

    refactor: conditionally render button for hidden menu items in header container

--- containers/header-container.tsx
+++ containers/header-container.tsx
@@ -51,34 +51,36 @@ const PageButtons = () => {
         </div>
       ))}
 
-      <button
-        className="flex flex-row gap-2 items-center text-gray-500 font-semibold disabled:text-white stroke-gray-500 fill-gray-500 disabled:stroke-blue-500 disabled:fill-blue-500"
-        disabled={false}
-        onClick={() => {
-          setShowDropdown((prev) => !prev)
-        }}
-      >
-        <span className={isMoreSelected ? 'text-white' : ''}>More</span>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="10"
-          height="6"
-          viewBox="0 0 10 6"
-          fill="none"
-          className="rotate-180"
+      {PAGE_BUTTONS.filter((page) => page.isHiddenMenu).length > 0 && (
+        <button
+          className="flex flex-row gap-2 items-center text-gray-500 font-semibold disabled:text-white stroke-gray-500 fill-gray-500 disabled:stroke-blue-500 disabled:fill-blue-500"
+          disabled={false}
+          onClick={() => {
+            setShowDropdown((prev) => !prev)
+          }}
         >
-          <path
-            d="M9 5L5 1L1 5"
-            stroke={isMoreSelected ? '#60A5FA' : '#9CA3AF'}
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          />
-        </svg>
-        <div className="relative">
-          {showDropdown ? <PageSelector /> : <></>}
-        </div>
-      </button>
+          <span className={isMoreSelected ? 'text-white' : ''}>More</span>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="10"
+            height="6"
+            viewBox="0 0 10 6"
+            fill="none"
+            className="rotate-180"
+          >
+            <path
+              d="M9 5L5 1L1 5"
+              stroke={isMoreSelected ? '#60A5FA' : '#9CA3AF'}
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            />
+          </svg>
+          <div className="relative">
+            {showDropdown ? <PageSelector /> : <></>}
+          </div>
+        </button>
+      )}
     </>
   )
 }
