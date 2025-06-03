00ce1c9888ff070bb87c02c5aeda204b37becd2e
graykode <nlkey2022@gmail.com>
2025-06-03 23:49:50 +0900

    refactor: simplify Web3AuthConnectorInstance and consolidate wallet creation logic

--- utils/web3auth/connector.ts
+++ utils/web3auth/connector.ts
@@ -1,18 +1,12 @@
 // Web3Auth Libraries
-import { CHAIN_NAMESPACES, UX_MODE, WEB3AUTH_NETWORK } from '@web3auth/base'
+import { CHAIN_NAMESPACES, WEB3AUTH_NETWORK } from '@web3auth/base'
 import { EthereumPrivateKeyProvider } from '@web3auth/ethereum-provider'
 import { Web3AuthConnector } from '@web3auth/web3auth-wagmi-connector'
-import { AuthAdapter } from '@web3auth/auth-adapter'
-import { Web3AuthNoModal } from '@web3auth/no-modal'
-import { LOGIN_PROVIDER_TYPE } from '@web3auth/auth'
+import { Web3Auth } from '@web3auth/modal'
 
 import { CHAIN_CONFIG } from '../../chain-configs'
 
-export default function Web3AuthConnectorInstance({
-  socialType,
-}: {
-  socialType: LOGIN_PROVIDER_TYPE
-}) {
+export default function Web3AuthConnectorInstance() {
   const chainConfig = {
     chainNamespace: CHAIN_NAMESPACES.EIP155,
     chainId: `0x${CHAIN_CONFIG.CHAIN.id.toString(16)}`,
@@ -28,34 +22,24 @@ export default function Web3AuthConnectorInstance({
     config: { chainConfig },
   })
 
-  const web3AuthInstance = new Web3AuthNoModal({
+  const web3AuthInstance = new Web3Auth({
     clientId: CHAIN_CONFIG.WEB3_AUTH_CLIENT_ID,
     privateKeyProvider,
     web3AuthNetwork: CHAIN_CONFIG.CHAIN.testnet
       ? WEB3AUTH_NETWORK.SAPPHIRE_DEVNET
       : WEB3AUTH_NETWORK.SAPPHIRE_MAINNET,
-  })
-
-  // Add openlogin adapter for customisations
-  const authAdapterInstance = new AuthAdapter({
-    adapterSettings: {
-      uxMode: UX_MODE.POPUP,
+    uiConfig: {
+      loginMethodsOrder: ['github', 'google'],
+      defaultLanguage: 'en',
+      modalZIndex: '2147483647',
+      logoLight: 'https://web3auth.io/images/w3a-L-Favicon-1.svg',
+      logoDark: 'https://web3auth.io/images/w3a-D-Favicon-1.svg',
+      uxMode: 'popup',
+      mode: 'dark',
     },
   })
-  web3AuthInstance.configureAdapter(authAdapterInstance)
 
   return Web3AuthConnector({
     web3AuthInstance,
-    name: socialType,
-    loginParams: {
-      loginProvider: socialType,
-      redirectUrl: window.location.origin,
-      extraLoginOptions: {
-        domain: window.location.hostname,
-      },
-    },
-    id: socialType,
-    type: socialType,
-    modalConfig: {},
   })
 }
--- utils/web3auth/index.ts
+++ utils/web3auth/index.ts
@@ -3,88 +3,17 @@ import { createConnector } from 'wagmi'
 
 import Web3AuthConnectorInstance from './connector'
 
-export const googleWallet = (): Wallet => ({
-  id: 'google_wallet',
-  name: 'Google',
-  iconUrl: async () =>
-    'https://raw.githubusercontent.com/edent/SuperTinyIcons/refs/heads/master/images/svg/google.svg',
+export const socialAccountWallet = (): Wallet => ({
+  id: 'sns_wallet',
+  name: 'Social Account',
+  iconUrl: async () => './snsWallet.png',
   iconBackground: '#fff',
   installed: true,
   createConnector: (walletDetails: WalletDetailsParams) =>
     createConnector(
       (config) =>
         ({
-          ...Web3AuthConnectorInstance({ socialType: 'google' })(config),
-          ...walletDetails,
-        }) as any,
-    ),
-})
-
-export const xWallet = (): Wallet => ({
-  id: 'x_wallet',
-  name: 'X',
-  iconUrl: async () =>
-    'https://raw.githubusercontent.com/edent/SuperTinyIcons/745841c3f62bdd0d1cf81d9a2ee4f9195729b459/images/svg/x.svg',
-  iconBackground: '#fff',
-  installed: true,
-  createConnector: (walletDetails: WalletDetailsParams) =>
-    createConnector(
-      (config) =>
-        ({
-          ...Web3AuthConnectorInstance({ socialType: 'twitter' })(config),
-          ...walletDetails,
-        }) as any,
-    ),
-})
-
-export const discordWallet = (): Wallet => ({
-  id: 'discord_wallet',
-  name: 'Discord',
-  iconUrl: async () =>
-    'https://raw.githubusercontent.com/edent/SuperTinyIcons/745841c3f62bdd0d1cf81d9a2ee4f9195729b459/images/svg/discord.svg',
-  iconBackground: '#fff',
-  installed: true,
-  createConnector: (walletDetails: WalletDetailsParams) =>
-    createConnector(
-      (config) =>
-        ({
-          ...Web3AuthConnectorInstance({ socialType: 'discord' })(config),
-          ...walletDetails,
-        }) as any,
-    ),
-})
-
-export const appleWallet = (): Wallet => ({
-  id: 'apple_wallet',
-  name: 'Apple',
-  iconUrl: async () =>
-    'https://raw.githubusercontent.com/edent/SuperTinyIcons/745841c3f62bdd0d1cf81d9a2ee4f9195729b459/images/svg/apple.svg',
-  iconBackground: '#fff',
-  installed: true,
-  createConnector: (walletDetails: WalletDetailsParams) =>
-    createConnector(
-      (config) =>
-        ({
-          ...Web3AuthConnectorInstance({ socialType: 'apple' })(config),
-          ...walletDetails,
-        }) as any,
-    ),
-})
-
-export const emailWallet = (): Wallet => ({
-  id: 'email_wallet',
-  name: 'Email',
-  iconUrl: async () =>
-    'https://raw.githubusercontent.com/edent/SuperTinyIcons/745841c3f62bdd0d1cf81d9a2ee4f9195729b459/images/svg/email.svg',
-  iconBackground: '#fff',
-  installed: true,
-  createConnector: (walletDetails: WalletDetailsParams) =>
-    createConnector(
-      (config) =>
-        ({
-          ...Web3AuthConnectorInstance({ socialType: 'email_passwordless' })(
-            config,
-          ),
+          ...Web3AuthConnectorInstance()(config),
           ...walletDetails,
         }) as any,
     ),
