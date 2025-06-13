73fc3d9bb38b50d8c5e318178917ac394ffbe291
graykode <nlkey2022@gmail.com>
2025-06-13 13:31:07 +0900

    feat: update aggregator configuration to include AggregatorRouterGateway and comment out CloberV2Aggregator

--- chain-configs/aggregators.ts
+++ chain-configs/aggregators.ts
@@ -10,14 +10,22 @@ import { MonorailAggregator } from '../model/aggregator/monorail'
 import { MadhouseAggregator } from '../model/aggregator/madhouse'
 
 export const aggregators: Aggregator[] = [
-  new CloberV2Aggregator(
-    getContractAddresses({ chainId: CHAIN_IDS.MONAD_TESTNET }).Controller,
-    monadTestnet,
-  ),
+  // new CloberV2Aggregator(
+  //   getContractAddresses({ chainId: CHAIN_IDS.MONAD_TESTNET }).Controller,
+  //   monadTestnet,
+  // ),
   // new OpenOceanAggregator(
   //   getAddress('0x6352a56caadC4F1E25CD6c75970Fa768A3304e64'),
   //   monadTestnet,
   // ),
+  new AggregatorRouterGateway(
+    getAddress('0xfD845859628946B317A78A9250DA251114FbD846'),
+    monadTestnet,
+    new CloberV2Aggregator(
+      getContractAddresses({ chainId: CHAIN_IDS.MONAD_TESTNET }).Controller,
+      monadTestnet,
+    ),
+  ),
   new AggregatorRouterGateway(
     getAddress('0xfD845859628946B317A78A9250DA251114FbD846'),
     monadTestnet,
