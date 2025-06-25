39de70bfa165b5e788683cc963e4502a6460bf1e
graykode <nlkey2022@gmail.com>
2025-06-25 15:46:04 +0900

    feat: conditionally render Wrap / Unwrap button based on whitelisted LP currencies

--- components/form/pool/add-liquidity-form.tsx
+++ components/form/pool/add-liquidity-form.tsx
@@ -1,4 +1,5 @@
 import React, { useMemo } from 'react'
+import { isAddressEqual } from 'viem'
 
 import { Prices } from '../../../model/prices'
 import { ActionButton, ActionButtonProps } from '../../button/action-button'
@@ -8,6 +9,7 @@ import { formatDollarValue, formatUnits } from '../../../utils/bigint'
 import { SlippageToggle } from '../../toggle/slippage-toggle'
 import { Chain } from '../../../model/chain'
 import { Pool } from '../../../model/pool'
+import { WHITELISTED_WRAPPED_LP_CURRENCIES } from '../../../chain-configs/pool'
 
 export const AddLiquidityForm = ({
   chain,
@@ -60,16 +62,20 @@ export const AddLiquidityForm = ({
         <div className="flex flex-row w-full text-white text-sm md:text-base font-bold">
           <div>Enter amount you’d like to add.</div>
 
-          <button
-            onClick={() => {
-              setShowLpWrapUnwrapModal(true)
-            }}
-            className="ml-auto w-fit sm:w-[140px] h-fit sm:h-8 px-2 sm:px-6 py-1 sm:py-4 bg-blue-500/25 rounded-xl shadow-[7.660800457000732px_7.660800457000732px_61.28640365600586px_0px_rgba(191,57,0,0.32)] inline-flex justify-center items-center"
-          >
-            <div className="text-nowrap opacity-90 text-center justify-center text-blue-400 text-[11px] sm:text-sm font-bold">
-              Wrap / Unwrap
-            </div>
-          </button>
+          {WHITELISTED_WRAPPED_LP_CURRENCIES.some((currency) =>
+            isAddressEqual(currency.address, pool.wrappedLpCurrency.address),
+          ) && (
+            <button
+              onClick={() => {
+                setShowLpWrapUnwrapModal(true)
+              }}
+              className="ml-auto w-fit sm:w-[140px] h-fit sm:h-8 px-2 sm:px-6 py-1 sm:py-4 bg-blue-500/25 rounded-xl shadow-[7.660800457000732px_7.660800457000732px_61.28640365600586px_0px_rgba(191,57,0,0.32)] inline-flex justify-center items-center"
+            >
+              <div className="text-nowrap opacity-90 text-center justify-center text-blue-400 text-[11px] sm:text-sm font-bold">
+                Wrap / Unwrap
+              </div>
+            </button>
+          )}
         </div>
         <div className="flex flex-col relative gap-4 self-stretch">
           <CurrencyAmountInput
