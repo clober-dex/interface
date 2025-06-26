1d5e65971b0836b57c3cc87691e8aa1ed3feee5c
graykode <nlkey2022@gmail.com>
2025-06-26 14:17:05 +0900

    test: update erc20 and native currency tests to use TIMEOUT constant and increase input amount

--- __tests__/quotes/erc20-to-erc20.spec.ts
+++ __tests__/quotes/erc20-to-erc20.spec.ts
@@ -3,57 +3,63 @@ import { formatUnits, parseUnits } from 'viem'
 import { CHAIN_CONFIG } from '../../chain-configs'
 import { aggregators } from '../../chain-configs/aggregators'
 
-import { getTokenBalance, publicClient, walletClient } from './utils'
+import { getTokenBalance, publicClient, TIMEOUT, walletClient } from './utils'
 
 describe('erc20 currency > erc20 currency', async () => {
-  const gasPrice = await publicClient.getGasPrice()
+  it(
+    'should execute all aggregator quotes and increase output balance',
+    async () => {
+      const gasPrice = await publicClient.getGasPrice()
 
-  const inputCurrency = CHAIN_CONFIG.WHITELISTED_CURRENCIES.find(
-    (currency) => currency.symbol === 'USDC',
-  )!
-  const outputCurrency = CHAIN_CONFIG.WHITELISTED_CURRENCIES.find(
-    (currency) => currency.symbol === 'USDT',
-  )!
-  const amountIn = parseUnits('1', inputCurrency.decimals)
+      const inputCurrency = CHAIN_CONFIG.WHITELISTED_CURRENCIES.find(
+        (currency) => currency.symbol === 'USDC',
+      )!
+      const outputCurrency = CHAIN_CONFIG.WHITELISTED_CURRENCIES.find(
+        (currency) => currency.symbol === 'USDT',
+      )!
+      const amountIn = parseUnits('100', inputCurrency.decimals)
 
-  for (let i = 0; i < aggregators.length; i++) {
-    try {
-      const quote = await aggregators[i].quote(
-        inputCurrency,
-        amountIn,
-        outputCurrency,
-        10,
-        gasPrice,
-        walletClient.account.address,
-      )
-      if (quote.transaction) {
-        const beforeBalance = await getTokenBalance({
-          tokenAddress: outputCurrency.address,
-          userAddress: walletClient.account.address,
-        })
-        const hash = await walletClient.sendTransaction(quote.transaction)
-        await publicClient.waitForTransactionReceipt({
-          hash,
-        })
-        console.log(
-          `Executed quote ${quote.aggregator.name} with hash: ${hash}`,
-        )
-        const afterBalance = await getTokenBalance({
-          tokenAddress: outputCurrency.address,
-          userAddress: walletClient.account.address,
-        })
-        expect(afterBalance).toBeGreaterThan(beforeBalance)
-        console.log(
-          `Quote ${quote.aggregator.name} executed successfully [${formatUnits(
-            beforeBalance,
-            outputCurrency.decimals,
-          )} -> ${formatUnits(afterBalance, outputCurrency.decimals)}]\n`,
-        )
+      for (let i = 0; i < aggregators.length; i++) {
+        try {
+          const quote = await aggregators[i].quote(
+            inputCurrency,
+            amountIn,
+            outputCurrency,
+            10,
+            gasPrice,
+            walletClient.account.address,
+          )
+          if (quote.transaction) {
+            const beforeBalance = await getTokenBalance({
+              tokenAddress: outputCurrency.address,
+              userAddress: walletClient.account.address,
+            })
+            const hash = await walletClient.sendTransaction(quote.transaction)
+            await publicClient.waitForTransactionReceipt({
+              hash,
+            })
+            console.log(
+              `Executed quote ${quote.aggregator.name} with hash: ${hash}`,
+            )
+            const afterBalance = await getTokenBalance({
+              tokenAddress: outputCurrency.address,
+              userAddress: walletClient.account.address,
+            })
+            expect(afterBalance).toBeGreaterThan(beforeBalance)
+            console.log(
+              `Quote ${quote.aggregator.name} executed successfully [${formatUnits(
+                beforeBalance,
+                outputCurrency.decimals,
+              )} -> ${formatUnits(afterBalance, outputCurrency.decimals)}]`,
+            )
+          }
+        } catch (error) {
+          console.warn(
+            `Quote ${aggregators[i].name} failed: ${error instanceof Error ? error.message : error}`,
+          )
+        }
       }
-    } catch (error) {
-      console.warn(
-        `Quote ${aggregators[i].name} failed: ${error instanceof Error ? error.message : error}\n`,
-      )
-    }
-  }
+    },
+    TIMEOUT,
+  )
 })
--- __tests__/quotes/native-to-erc20.spec.ts
+++ __tests__/quotes/native-to-erc20.spec.ts
@@ -3,58 +3,64 @@ import { formatUnits, parseUnits, zeroAddress } from 'viem'
 import { CHAIN_CONFIG } from '../../chain-configs'
 import { aggregators } from '../../chain-configs/aggregators'
 
-import { getTokenBalance, publicClient, walletClient } from './utils'
+import { getTokenBalance, publicClient, TIMEOUT, walletClient } from './utils'
 
 describe('native currency > erc20 currency', async () => {
-  const gasPrice = await publicClient.getGasPrice()
+  it(
+    'should execute all aggregator quotes and increase output balance',
+    async () => {
+      const gasPrice = await publicClient.getGasPrice()
 
-  const inputCurrency = {
-    ...CHAIN_CONFIG.CHAIN.nativeCurrency,
-    address: zeroAddress,
-  }
-  const outputCurrency = CHAIN_CONFIG.WHITELISTED_CURRENCIES.find(
-    (currency) => currency.symbol === 'USDC',
-  )!
-  const amountIn = parseUnits('1', inputCurrency.decimals)
-
-  for (let i = 0; i < aggregators.length; i++) {
-    try {
-      const quote = await aggregators[i].quote(
-        inputCurrency,
-        amountIn,
-        outputCurrency,
-        10,
-        gasPrice,
-        walletClient.account.address,
-      )
-      if (quote.transaction) {
-        const beforeBalance = await getTokenBalance({
-          tokenAddress: outputCurrency.address,
-          userAddress: walletClient.account.address,
-        })
-        const hash = await walletClient.sendTransaction(quote.transaction)
-        await publicClient.waitForTransactionReceipt({
-          hash,
-        })
-        console.log(
-          `Executed quote ${quote.aggregator.name} with hash: ${hash}`,
-        )
-        const afterBalance = await getTokenBalance({
-          tokenAddress: outputCurrency.address,
-          userAddress: walletClient.account.address,
-        })
-        expect(afterBalance).toBeGreaterThan(beforeBalance)
-        console.log(
-          `Quote ${quote.aggregator.name} executed successfully [${formatUnits(
-            beforeBalance,
-            outputCurrency.decimals,
-          )} -> ${formatUnits(afterBalance, outputCurrency.decimals)}]\n`,
-        )
+      const inputCurrency = {
+        ...CHAIN_CONFIG.CHAIN.nativeCurrency,
+        address: zeroAddress,
       }
-    } catch (error) {
-      console.warn(
-        `Quote ${aggregators[i].name} failed: ${error instanceof Error ? error.message : error}\n`,
-      )
-    }
-  }
+      const outputCurrency = CHAIN_CONFIG.WHITELISTED_CURRENCIES.find(
+        (currency) => currency.symbol === 'USDC',
+      )!
+      const amountIn = parseUnits('100', inputCurrency.decimals)
+
+      for (let i = 0; i < aggregators.length; i++) {
+        try {
+          const quote = await aggregators[i].quote(
+            inputCurrency,
+            amountIn,
+            outputCurrency,
+            10,
+            gasPrice,
+            walletClient.account.address,
+          )
+          if (quote.transaction) {
+            const beforeBalance = await getTokenBalance({
+              tokenAddress: outputCurrency.address,
+              userAddress: walletClient.account.address,
+            })
+            const hash = await walletClient.sendTransaction(quote.transaction)
+            await publicClient.waitForTransactionReceipt({
+              hash,
+            })
+            console.log(
+              `Executed quote ${quote.aggregator.name} with hash: ${hash}`,
+            )
+            const afterBalance = await getTokenBalance({
+              tokenAddress: outputCurrency.address,
+              userAddress: walletClient.account.address,
+            })
+            expect(afterBalance).toBeGreaterThan(beforeBalance)
+            console.log(
+              `Quote ${quote.aggregator.name} executed successfully [${formatUnits(
+                beforeBalance,
+                outputCurrency.decimals,
+              )} -> ${formatUnits(afterBalance, outputCurrency.decimals)}]`,
+            )
+          }
+        } catch (error) {
+          console.warn(
+            `Quote ${aggregators[i].name} failed: ${error instanceof Error ? error.message : error}`,
+          )
+        }
+      }
+    },
+    TIMEOUT,
+  )
 })
