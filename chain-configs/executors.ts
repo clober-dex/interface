07b99b8b7dbbc332b742ecc31b365fe14a293246
graykode <nlkey2022@gmail.com>
2025-07-24 11:49:00 +0900

    feat: update FastLaneExecutor address for monadTestnet

--- chain-configs/executors.ts
+++ chain-configs/executors.ts
@@ -6,7 +6,7 @@ import { FastLaneExecutor } from '../model/executor/fast-lane'
 
 export const executors: Executor[] = [
   new FastLaneExecutor(
-    getAddress('0x4a730A56344873FB28f7C3d65A67Fea56f5e0F46'),
+    getAddress('0xbB010Cb7e71D44d7323aE1C267B333A48D05907C'),
     monadTestnet,
   ),
 ]
