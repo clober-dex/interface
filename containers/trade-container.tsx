6ae7930761ad0eb49546f7c2a19e2c553fd04245
graykode <nlkey2022@gmail.com>
2025-07-28 12:55:12 +0900

    fix: adjust gap spacing in latest swaps container for improved layout

--- containers/trade-container.tsx
+++ containers/trade-container.tsx
@@ -111,7 +111,7 @@ const MetaAggregatorInfo = ({
         </div>
       </div>
 
-      <div className="absolute flex bottom-4 w-full px-8 gap-2.5 flex-col max-h-[200px] overflow-y-scroll">
+      <div className="absolute flex bottom-4 w-full px-8 gap-3 flex-col max-h-[200px] overflow-y-scroll">
         <AnimatePresence initial={false}>
           {latestSwaps
             .filter(
@@ -131,7 +131,7 @@ const MetaAggregatorInfo = ({
                 <a
                   href={`${chain.blockExplorers?.default?.url}/tx/${latestSwap.transaction.id}`}
                   target="_blank"
-                  className="h-8 flex w-full px-3 py-1.5 bg-[#819cff]/10 rounded-[32px] shadow-[0px_0px_6px_0px_rgba(96,165,250,0.25)] justify-start items-center gap-2 overflow-hidden"
+                  className="h-8 max-h-8 flex w-full px-3 py-1.5 bg-[#819cff]/10 rounded-[32px] shadow-[0px_0px_6px_0px_rgba(96,165,250,0.25)] justify-start items-center gap-2 overflow-hidden"
                   rel="noopener"
                 >
                   <div className="justify-start text-blue-400 font-semibold text-nowrap">
