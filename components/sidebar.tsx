8a5419b8729347c4999c2a8b04db731802629fff
graykode <nlkey2022@gmail.com>
2025-08-26 15:42:57 +0900

    style: update z-index values for improved layering in various components

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -11,18 +11,16 @@ const Sidebar = ({
   router: NextRouter
 } & React.PropsWithChildren) => {
   return (
-    <div className="invisible md:visible fixed flex top-0 left-0 h-screen items-center justify-center w-20 bg-[#151517] border-r border-[#2d2d2e] border-solid">
+    <div className="invisible md:visible fixed z-[1] flex top-0 left-0 h-screen items-center justify-center w-20 bg-[#151517] border-r border-[#2d2d2e] border-solid">
       <div className="flex flex-col justify-center items-center gap-3">
         {PAGE_BUTTONS.filter((page) => !page.isHiddenMenu).map((page) => (
           <div key={page.path}>
-            <div className="self-stretch p-2 rounded-[10px] inline-flex justify-start items-center gap-2.5">
-              <PageButton
-                disabled={router.pathname.includes(page.path)}
-                onClick={() => router.push(page.path)}
-              >
-                {page.icon}
-              </PageButton>
-            </div>
+            <PageButton
+              disabled={router.pathname.includes(page.path)}
+              onClick={() => router.push(page.path)}
+            >
+              {page.icon}
+            </PageButton>
           </div>
         ))}
       </div>
