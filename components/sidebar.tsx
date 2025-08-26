fc4fc713aa7cf18cf9f9bdbd274a25f44772c09d
graykode <nlkey2022@gmail.com>
2025-08-26 15:27:56 +0900

    style: increase SVG sizes for improved visibility and consistency

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,14 +1,31 @@
 import React from 'react'
 import { NextRouter } from 'next/router'
 
+import { PAGE_BUTTONS } from '../chain-configs/page-button'
+
+import { PageButton } from './button/page-button'
+
 const Sidebar = ({
   router,
 }: {
   router: NextRouter
 } & React.PropsWithChildren) => {
   return (
-    <div className="invisible md:visible absolute top-0 left-0 h-full w-20 bg-[#151517] border-r border-[#2d2d2e] border-solid">
-      Test
+    <div className="invisible md:visible fixed flex top-0 left-0 h-screen items-center justify-center w-20 bg-[#151517] border-r border-[#2d2d2e] border-solid">
+      <div className="flex flex-col justify-center items-center gap-3">
+        {PAGE_BUTTONS.filter((page) => !page.isHiddenMenu).map((page) => (
+          <div key={page.path}>
+            <div className="self-stretch p-2 rounded-[10px] inline-flex justify-start items-center gap-2.5">
+              <PageButton
+                disabled={router.pathname.includes(page.path)}
+                onClick={() => router.push(page.path)}
+              >
+                {page.icon}
+              </PageButton>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
