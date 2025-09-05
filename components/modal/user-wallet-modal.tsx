af5995b2f8601716c439bea4496171a789b4acff
graykode <nlkey2022@gmail.com>
2025-09-05 18:01:37 +0900

    style: update font weights for consistency; adjust dimensions and spacing in user wallet modal

--- components/modal/user-wallet-modal.tsx
+++ components/modal/user-wallet-modal.tsx
@@ -112,20 +112,20 @@ export const UserWalletModal = ({
         setIsCopyToast={setIsCopyToast}
         durationInMs={1300}
       >
-        <div className="w-[240px] items-center justify-center flex flex-row gap-1.5 text-white text-sm font-semibold">
+        <div className="w-[240px] items-center justify-center flex flex-row gap-1.5 text-white text-sm font-medium">
           <ClipboardSvg />
           Address copied to clipboard
         </div>
       </Toast>
 
       <div className="flex flex-col max-h-[460px] sm:max-h-[576px]">
-        <h1 className="flex font-bold mb-6 sm:text-xl items-center justify-center w-full">
+        <h1 className="flex font-semibold mb-6 sm:text-xl items-center justify-center w-full">
           My Wallet
         </h1>
         <div className="flex flex-col justify-start items-start gap-6">
           <div className="self-stretch px-4 py-2 sm:py-3 bg-gray-800 rounded-xl flex justify-center items-center gap-[17px] h-full">
             <div className="flex flex-row gap-2 h-full items-center">
-              <div className="flex w-8 sm:w-10 h-4 sm:h-6 relative items-center">
+              <div className="flex w-6 sm:w-10 h-4 sm:h-6 relative items-center">
                 {walletIconUrl ? (
                   <img
                     src={walletIconUrl}
@@ -144,14 +144,12 @@ export const UserWalletModal = ({
                 />
               </div>
 
-              <span
-                className={`block text-white text-sm sm:text-base font-semibold`}
-              >
+              <span className="block text-white text-sm sm:text-base font-medium">
                 {ens ?? formatAddress(userAddress || '', 6)}
               </span>
             </div>
 
-            <div className="flex flex-row gap-1.5 sm:gap-1 ml-auto">
+            <div className="flex flex-row gap-1 ml-auto">
               <button
                 onClick={async () => {
                   await handleCopyClipBoard(userAddress)
@@ -161,13 +159,15 @@ export const UserWalletModal = ({
               >
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
+                  width="12"
+                  height="12"
                   viewBox="0 0 12 12"
                   fill="none"
-                  className="stroke-white w-3 sm:w-4 h-4 sm:h-4"
+                  className="w-2.5 h-2.5 sm:w-3 sm:h-3"
                 >
                   <rect
-                    x="3.85715"
-                    y="3.85715"
+                    x="3.85718"
+                    y="3.85718"
                     width="7.14286"
                     height="7.14286"
                     stroke="white"
@@ -180,6 +180,7 @@ export const UserWalletModal = ({
                   />
                 </svg>
               </button>
+
               <button
                 onClick={() =>
                   window.open(`${explorerUrl}/address/${userAddress}`, '_blank')
@@ -192,6 +193,7 @@ export const UserWalletModal = ({
                   height="16"
                   viewBox="0 0 16 16"
                   fill="none"
+                  className="w-3 h-3 sm:w-4 sm:h-4"
                 >
                   <path
                     d="M12 13L3 13L3 4"
@@ -214,6 +216,7 @@ export const UserWalletModal = ({
                   />
                 </svg>
               </button>
+
               <button
                 onClick={disconnectAsync}
                 className="p-1 sm:p-2 bg-gray-700 rounded-lg flex flex-col items-center justify-center w-6 sm:w-8 h-6 sm:h-8"
@@ -224,9 +227,10 @@ export const UserWalletModal = ({
                   height="16"
                   viewBox="0 0 16 16"
                   fill="none"
+                  className="w-3 h-3 sm:w-4 sm:h-4"
                 >
                   <path
-                    d="M5.42908 14.2073C3.95842 14.2073 2.63256 13.3214 2.06967 11.9627C1.50678 10.604 1.81769 9.04007 2.85745 8L4.40072 6.45672L5.42908 7.48509L3.88654 9.02763C3.33531 9.57886 3.12003 10.3823 3.32179 11.1353C3.52356 11.8883 4.11171 12.4764 4.8647 12.6782C5.6177 12.88 6.42113 12.6647 6.97236 12.1135L8.5149 10.5709L9.54327 11.6L8.00072 13.1425C7.32005 13.8266 6.39407 14.2099 5.42908 14.2073ZM5.94327 11.0851L4.9149 10.0567L10.0574 4.91418L11.0858 5.94254L5.94399 11.0844L5.94327 11.0851ZM11.6007 9.54254L10.5716 8.51418L12.1142 6.97163C12.6729 6.42206 12.894 5.61503 12.6933 4.85744C12.4926 4.09985 11.901 3.50811 11.1434 3.30723C10.3859 3.10635 9.57879 3.32721 9.02908 3.88582L7.48581 5.42836L6.45745 4.4L8.00072 2.85672C9.4226 1.44725 11.7163 1.45227 13.132 2.86796C14.5477 4.28365 14.5527 6.57739 13.1433 7.99927L11.6007 9.54181V9.54254Z"
+                    d="M5.42908 14.2072C3.95842 14.2072 2.63256 13.3214 2.06967 11.9627C1.50678 10.604 1.81769 9.04003 2.85745 7.99996L4.40072 6.45669L5.42908 7.48505L3.88654 9.0276C3.33531 9.57882 3.12003 10.3823 3.32179 11.1352C3.52356 11.8882 4.11171 12.4764 4.8647 12.6782C5.6177 12.8799 6.42113 12.6646 6.97236 12.1134L8.5149 10.5709L9.54327 11.6L8.00072 13.1425C7.32005 13.8265 6.39407 14.2099 5.42908 14.2072ZM5.94327 11.085L4.9149 10.0567L10.0574 4.91414L11.0858 5.9425L5.94399 11.0843L5.94327 11.085ZM11.6007 9.5425L10.5716 8.51414L12.1142 6.9716C12.6729 6.42202 12.894 5.61499 12.6933 4.8574C12.4926 4.09981 11.901 3.50807 11.1434 3.30719C10.3859 3.10632 9.57879 3.32717 9.02908 3.88578L7.48581 5.42832L6.45745 4.39996L8.00072 2.85669C9.4226 1.44721 11.7163 1.45223 13.132 2.86792C14.5477 4.28362 14.5527 6.57735 13.1433 7.99923L11.6007 9.54178V9.5425Z"
                     fill="#EF4444"
                   />
                   <rect
@@ -239,26 +243,26 @@ export const UserWalletModal = ({
                   />
                   <rect
                     x="11.9881"
-                    y="11.2232"
+                    y="11.2231"
                     width="1.45454"
                     height="2.90909"
-                    transform="rotate(-75 11.9881 11.2232)"
+                    transform="rotate(-75 11.9881 11.2231)"
                     fill="#EF4444"
                   />
                   <rect
                     x="5.81818"
-                    y="3.53713"
+                    y="3.53711"
                     width="1.45455"
                     height="2.90909"
-                    transform="rotate(165 5.81818 3.53713)"
+                    transform="rotate(165 5.81818 3.53711)"
                     fill="#EF4444"
                   />
                   <rect
                     x="4.0119"
-                    y="4.55205"
+                    y="4.552"
                     width="1.45455"
                     height="2.90909"
-                    transform="rotate(105 4.0119 4.55205)"
+                    transform="rotate(105 4.0119 4.552)"
                     fill="#EF4444"
                   />
                 </svg>
@@ -269,7 +273,7 @@ export const UserWalletModal = ({
             <div className="flex gap-10">
               <button
                 onClick={() => setTab('my-tokens')}
-                className={`pb-2 text-sm sm:text-base font-semibold transition-all duration-150 ${
+                className={`pb-2 text-sm sm:text-base font-medium transition-all duration-150 ${
                   tab === 'my-tokens'
                     ? 'text-white border-b-2 border-white'
                     : 'text-gray-500 border-b-2 border-transparent'
@@ -279,7 +283,7 @@ export const UserWalletModal = ({
               </button>
               <button
                 onClick={() => setTab('my-transactions')}
-                className={`pb-2 text-sm sm:text-base font-semibold transition-all duration-150 ${
+                className={`pb-2 text-sm sm:text-base font-medium transition-all duration-150 ${
                   tab === 'my-transactions'
                     ? 'text-white border-b-2 border-white'
                     : 'text-gray-500 border-b-2 border-transparent'
@@ -292,7 +296,7 @@ export const UserWalletModal = ({
         </div>
 
         {tab === 'my-tokens' && (
-          <div className="sticky top-0 z-10 text-center justify-start text-white text-[28px] font-semibold mb-4 mt-2 py-2">
+          <div className="sticky top-0 z-10 text-center justify-start text-white text-[28px] font-medium mb-4 mt-2 py-2">
             ${formatTinyNumber(portfolioUSD)}
           </div>
         )}
@@ -339,16 +343,16 @@ export const UserWalletModal = ({
                         <div className="w-60 flex justify-start items-center gap-3">
                           <CurrencyIcon chain={chain} currency={currency} />
                           <div className="text-nowrap flex-1 flex flex-col justify-center items-start gap-0.5">
-                            <div className="w-[200px] max-w-[200px] overflow-x-scroll text-start justify-start text-white text-sm font-semibold">
+                            <div className="w-[200px] max-w-[200px] overflow-x-scroll text-start justify-start text-white text-sm font-medium">
                               {currency.symbol}
                             </div>
-                            <div className="text-center justify-start text-[#a8afbc] text-xs font-semibold">
+                            <div className="text-center justify-start text-[#a8afbc] text-xs font-medium">
                               ${formatTinyNumber(prices[currency.address])}{' '}
                             </div>
                           </div>
                         </div>
                         <div className="flex-1 flex flex-col justify-center items-start gap-1">
-                          <div className="text-center justify-start text-white text-sm font-semibold">
+                          <div className="text-center justify-start text-white text-sm font-medium">
                             {formatWithCommas(
                               formatUnits(
                                 balances[currency.address],
@@ -357,7 +361,7 @@ export const UserWalletModal = ({
                               ),
                             )}
                           </div>
-                          <div className="text-center justify-start text-[#a9b0bc] text-xs font-semibold">
+                          <div className="text-center justify-start text-[#a9b0bc] text-xs font-medium">
                             {prices[currency.address] ? (
                               <div className="text-gray-500 text-xs">
                                 {formatDollarValue(
@@ -378,7 +382,7 @@ export const UserWalletModal = ({
                           }}
                           className="px-3 py-2 bg-blue-400/20 rounded-lg flex justify-center items-center gap-2.5"
                         >
-                          <div className="justify-start text-blue-300 text-[13px] font-semibold">
+                          <div className="justify-start text-blue-300 text-[13px] font-medium">
                             Send
                           </div>
                         </button>
@@ -402,7 +406,7 @@ export const UserWalletModal = ({
                       className="flex flex-col w-full border-t border-[#2f313d] border-solid"
                       key={transaction.txHash}
                     >
-                      <div className="flex pt-2 justify-start text-gray-500 text-sm font-bold">
+                      <div className="flex pt-2 justify-start text-gray-500 text-sm font-semibold">
                         {getTimeAgo(transaction.timestamp, cache)}
                       </div>
                       <UserTransactionCard
