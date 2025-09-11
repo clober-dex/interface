c63fb0af8c18c74d5f1bf05ca6ee863ed70a705c
graykode <nlkey2022@gmail.com>
2025-09-11 13:39:32 +0900

    feat: update slippage input default value to improve trading experience

--- containers/pool/pool-container.tsx
+++ containers/pool/pool-container.tsx
@@ -258,7 +258,7 @@ export const PoolContainer = () => {
             </div>
 
             <div className="relative flex justify-center w-full h-full lg:h-[460px] lg:bg-[#17181e] lg:outline lg:outline-1 lg:outline-offset-[-1px] lg:outline-[#272930]">
-              <div className="lg:absolute lg:top-0 lg:overflow-x-scroll w-full h-full items-center flex flex-1 flex-col md:grid md:grid-cols-2 lg:flex gap-3 lg:gap-0">
+              <div className="lg:overflow-x-scroll w-full h-auto items-center flex flex-1 flex-col md:grid md:grid-cols-2 lg:flex gap-3 lg:gap-0">
                 {Object.entries(lpBalances).flatMap(([poolKey, amount]) => {
                   const poolSnapshot = poolSnapshots.find(
                     ({ key }) => key === poolKey,
