7072012fa4d863adb08489892721aabb5cb5f5ca
graykode <nlkey2022@gmail.com>
2025-09-29 16:53:39 +0900

    refactor: update button background color to use Tailwind CSS class for consistency

--- components/button/page-button.tsx
+++ components/button/page-button.tsx
@@ -17,7 +17,7 @@ export const PageButton = ({
       disabled={disabled}
       onClick={onClick}
       className={clsx(
-        'group flex flex-row gap-2 items-center text-gray-500 disabled:text-white stroke-gray-500 fill-gray-500 disabled:stroke-blue-500 disabled:fill-blue-500 rounded-[10px] disabled:bg-[#367fff]/20 p-2 font-semibold md:text-sm md:font-medium',
+        'group flex flex-row gap-2 items-center text-gray-500 disabled:text-white stroke-gray-500 fill-gray-500 disabled:stroke-blue-500 disabled:fill-blue-500 rounded-[10px] disabled:bg-blue-500/20 p-2 font-semibold md:text-sm md:font-medium',
         className,
       )}
     >
