8a21dc9d6ca78b5d4b1b397456105c6dbd1ef0a8
graykode <nlkey2022@gmail.com>
2025-11-10 14:00:44 +0900

    feat: enhance user transaction card with chain icon and footer support

--- components/card/user-transaction-card.tsx
+++ components/card/user-transaction-card.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 
 import { Transaction } from '../../contexts/transaction-context'
 import { CurrencyIcon } from '../icon/currency-icon'
+import ChainIcon from '../icon/chain-icon'
 
 const UserTransactionCard = ({
   transaction,
@@ -14,10 +15,13 @@ const UserTransactionCard = ({
   return (
     <button
       className="self-stretch pt-2 flex flex-col w-full justify-start items-start gap-3 cursor-pointer"
-      onClick={() =>
-        explorerUrl &&
-        window.open(`${explorerUrl}/tx/${transaction.txHash}`, '_blank')
-      }
+      onClick={() => {
+        if (transaction.externalLink) {
+          window.open(transaction.externalLink, '_blank')
+        } else if (explorerUrl) {
+          window.open(`${explorerUrl}/tx/${transaction.txHash}`, '_blank')
+        }
+      }}
     >
       <div className="self-stretch flex justify-between items-center gap-0.5">
         <div className="justify-center text-white text-start text-xs sm:text-sm font-semibold mr-auto text-nowrap overflow-x-scroll">
@@ -90,15 +94,30 @@ const UserTransactionCard = ({
                 .map((field, index) => (
                   <div
                     key={`transaction-${transaction.txHash}-in-${index}`}
-                    className="flex w-full items-center justify-between bg-gray-800 px-2 py-1.5 text-xs sm:text-sm rounded-lg"
+                    className="flex w-full items-center justify-between bg-gray-800 px-2 py-1.5 text-xs sm:text-sm rounded-lg h-9"
                   >
                     <div className="flex items-center gap-1.5 truncate">
-                      {field.currency ? (
-                        <CurrencyIcon
-                          chain={transaction.chain}
-                          currency={field.currency}
-                          className="w-4 h-4 rounded-full"
-                        />
+                      {field.currency && transaction.chain ? (
+                        field.chain ? (
+                          <div className="w-6 h-6 relative">
+                            <CurrencyIcon
+                              chain={transaction.chain}
+                              currency={field.currency}
+                              className="w-5 h-5 absolute left-0 top-1 z-[1] rounded-xl bg-gray-300 aspect-square"
+                            />
+
+                            <ChainIcon
+                              chain={field.chain}
+                              className="w-3 h-3 absolute left-3 top-0 z-[2] rounded-xl aspect-square bg-white p-0.5"
+                            />
+                          </div>
+                        ) : (
+                          <CurrencyIcon
+                            chain={transaction.chain}
+                            currency={field.currency}
+                            className="w-5 h-5 rounded-full"
+                          />
+                        )
                       ) : (
                         <></>
                       )}
@@ -115,7 +134,7 @@ const UserTransactionCard = ({
           <div className="flex flex-1 w-full self-stretch justify-start items-start gap-1">
             {transaction.fields.filter((field) => field.direction === 'in')
               .length > 0 && (
-              <div className="flex text-sm w-9 items-center justify-center bg-red-500 bg-opacity-10 font-bold text-red-500 rounded-lg h-7">
+              <div className="flex text-sm w-9 items-center justify-center bg-red-500 bg-opacity-10 font-bold text-red-500 rounded-lg h-9">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   viewBox="0 0 12 4"
@@ -138,15 +157,30 @@ const UserTransactionCard = ({
                 .map((field, index) => (
                   <div
                     key={`transaction-${transaction.txHash}-in-${index}`}
-                    className="flex w-full items-center justify-between bg-gray-800 px-2 py-1.5 text-xs sm:text-sm rounded-lg"
+                    className="flex w-full items-center justify-between bg-gray-800 px-2 py-1.5 text-xs sm:text-sm rounded-lg h-9"
                   >
                     <div className="flex items-center gap-1.5 truncate">
-                      {field.currency ? (
-                        <CurrencyIcon
-                          chain={transaction.chain}
-                          currency={field.currency}
-                          className="w-4 h-4 rounded-full"
-                        />
+                      {field.currency && transaction.chain ? (
+                        field.chain ? (
+                          <div className="w-6 h-6 relative">
+                            <CurrencyIcon
+                              chain={transaction.chain}
+                              currency={field.currency}
+                              className="w-5 h-5 absolute left-0 top-1 z-[1] rounded-xl bg-gray-300 aspect-square"
+                            />
+
+                            <ChainIcon
+                              chain={field.chain}
+                              className="w-3 h-3 absolute left-3 top-0 z-[2] rounded-xl aspect-square bg-white p-0.5"
+                            />
+                          </div>
+                        ) : (
+                          <CurrencyIcon
+                            chain={transaction.chain}
+                            currency={field.currency}
+                            className="w-5 h-5 rounded-full"
+                          />
+                        )
                       ) : (
                         <></>
                       )}
@@ -163,7 +197,7 @@ const UserTransactionCard = ({
           <div className="flex flex-1 w-full self-stretch justify-start items-start gap-1">
             {transaction.fields.filter((field) => field.direction === 'out')
               .length > 0 && (
-              <div className="flex text-sm w-9 items-center justify-center bg-green-500 bg-opacity-10 font-bold text-green-500 rounded-lg h-7">
+              <div className="flex text-sm w-9 items-center justify-center bg-green-500 bg-opacity-10 font-bold text-green-500 rounded-lg h-9">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   viewBox="0 0 16 16"
@@ -186,15 +220,30 @@ const UserTransactionCard = ({
                 .map((field, index) => (
                   <div
                     key={`transaction-${transaction.txHash}-out-${index}`}
-                    className="flex w-full items-center justify-between bg-gray-800 px-2 py-1.5 text-xs sm:text-sm rounded-lg"
+                    className="flex w-full items-center justify-between bg-gray-800 px-2 py-1.5 text-xs sm:text-sm rounded-lg h-9"
                   >
                     <div className="flex items-center gap-1.5 truncate">
-                      {field.currency ? (
-                        <CurrencyIcon
-                          chain={transaction.chain}
-                          currency={field.currency}
-                          className="w-4 h-4 rounded-full"
-                        />
+                      {field.currency && transaction.chain ? (
+                        field.chain ? (
+                          <div className="w-6 h-6 relative">
+                            <CurrencyIcon
+                              chain={transaction.chain}
+                              currency={field.currency}
+                              className="w-5 h-5 absolute left-0 top-1 z-[1] rounded-xl bg-gray-300 aspect-square"
+                            />
+
+                            <ChainIcon
+                              chain={field.chain}
+                              className="w-3 h-3 absolute left-3 top-0 z-[2] rounded-xl aspect-square bg-white p-0.5"
+                            />
+                          </div>
+                        ) : (
+                          <CurrencyIcon
+                            chain={transaction.chain}
+                            currency={field.currency}
+                            className="w-5 h-5 rounded-full"
+                          />
+                        )
                       ) : (
                         <></>
                       )}
@@ -207,6 +256,12 @@ const UserTransactionCard = ({
                 ))}
             </div>
           </div>
+
+          {transaction.footer && (
+            <span className="text-xs flex w-full justify-end mt-1">
+              {transaction.footer}
+            </span>
+          )}
         </div>
       )}
     </button>
