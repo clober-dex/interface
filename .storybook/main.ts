4855e695ae15b22347c1d3d7c4eb49d40b78d521
graykode <nlkey2022@gmail.com>
2025-11-11 17:25:00 +0900

    feat: update dependencies in package.json for compatibility and performance

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,28 +1,38 @@
 import type { StorybookConfig } from '@storybook/nextjs'
+import path from 'path'
 
-// @ts-ignore
 const config: StorybookConfig = {
   stories: ['../components/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
-
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-onboarding',
     '@storybook/addon-interactions',
   ],
-
   framework: {
     name: '@storybook/nextjs',
     options: {},
   },
+  docs: { autodocs: 'tag' },
+  typescript: { reactDocgen: 'react-docgen-typescript' },
 
-  docs: {
-    autodocs: 'tag',
+  webpackFinal: async (config) => {
+    config.plugins = config.plugins?.filter(
+      (plugin) => plugin?.constructor?.name !== 'HtmlWebpackPlugin'
+    )
+
+    // Tamagui / RN alias
+    config.resolve = {
+      ...(config.resolve ?? {}),
+      alias: {
+        ...(config.resolve?.alias ?? {}),
+        '@tamagui/core': path.resolve(__dirname, '../node_modules/@tamagui/core'),
+        '@tamagui/web': path.resolve(__dirname, '../node_modules/@tamagui/web'),
+        'react-native': 'react-native-web',
+      },
+    }
+    return config
   },
-
-  typescript: {
-    reactDocgen: 'react-docgen-typescript'
-  },
-
 }
+
 export default config
