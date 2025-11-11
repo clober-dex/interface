64927e3f29ea066ab1a50cee97acd6f530fd0505
graykode <nlkey2022@gmail.com>
2025-11-11 17:42:50 +0900

    feat: simplify Storybook configuration by removing unnecessary webpackFinal customizations

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,37 +1,27 @@
 import type { StorybookConfig } from '@storybook/nextjs'
-import path from 'path'
 
+// @ts-ignore
 const config: StorybookConfig = {
   stories: ['../components/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-onboarding',
     '@storybook/addon-interactions',
   ],
+
   framework: {
     name: '@storybook/nextjs',
     options: {},
   },
-  docs: { autodocs: 'tag' },
-  typescript: { reactDocgen: 'react-docgen-typescript' },
 
-  webpackFinal: async (config) => {
-    config.plugins = config.plugins?.filter(
-      (plugin) => plugin?.constructor?.name !== 'HtmlWebpackPlugin'
-    )
+  docs: {
+    autodocs: 'tag',
+  },
 
-    // Tamagui / RN alias
-    config.resolve = {
-      ...(config.resolve ?? {}),
-      alias: {
-        ...(config.resolve?.alias ?? {}),
-        '@tamagui/core': path.resolve(__dirname, '../node_modules/@tamagui/core'),
-        '@tamagui/web': path.resolve(__dirname, '../node_modules/@tamagui/web'),
-        'react-native': 'react-native-web',
-      },
-    }
-    return config
+  typescript: {
+    reactDocgen: 'react-docgen-typescript'
   },
 }
 
