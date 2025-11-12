5dcd99fba0acf58ae0e1c68f40db6ba5d576304a
graykode <nlkey2022@gmail.com>
2025-11-12 17:24:17 +0900

    feat: use yarn

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -11,10 +11,7 @@ const config: StorybookConfig = {
     '@storybook/addon-interactions',
   ],
 
-  framework: {
-    name: '@storybook/nextjs',
-    options: {},
-  },
+  framework: '@storybook/nextjs',
 
   docs: {
     autodocs: 'tag',
@@ -22,6 +19,13 @@ const config: StorybookConfig = {
 
   typescript: {
     reactDocgen: 'react-docgen-typescript'
+  },
+
+  webpackFinal: async (config) => {
+    config.plugins = config.plugins?.filter(
+      (plugin) => plugin?.constructor.name !== 'HtmlWebpackPlugin'
+    )
+    return config
   },
 }
 
