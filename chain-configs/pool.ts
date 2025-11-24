3af6bcb53f660a2c77e736008ae3392edf8dc549
graykode <nlkey2022@gmail.com>
2025-11-25 00:53:49 +0900

    feat: add new pool key to the whitelisted pools

--- chain-configs/pool.ts
+++ chain-configs/pool.ts
@@ -3,4 +3,9 @@ import { Currency } from '../model/currency'
 export const WHITELISTED_POOL_KEY_AND_WRAPPED_CURRENCIES: {
   poolKey: `0x${string}`
   wrappedLpCurrency?: Currency
-}[] = []
+}[] = [
+  {
+    poolKey:
+      '0xbe1bdd34a1c3fdc238c581aa52ffd5f5ebb61e229ee65197f00679df7d51beea',
+  },
+]
