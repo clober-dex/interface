5298ce73415edf58699a09d1aaaa4b1b2336a17d
graykode <nlkey2022@gmail.com>
2025-11-24 17:01:51 +0900

    Revert "feat: remove middleware"
    This reverts commit 4ba908590bfa2d6f560e8d1636bdaacf59673271.

---
+++ middleware.ts
@@ -0,0 +1,25 @@
+// TODO: remove this file if no longer needed
+// middleware.ts
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+
+export function middleware(req: NextRequest) {
+  const basicAuth = req.headers.get('authorization')
+
+  if (basicAuth) {
+    const authValue = basicAuth.split(' ')[1]
+    const [user, pwd] = atob(authValue).split(':')
+
+    if (
+      user === process.env.BASIC_AUTH_USER &&
+      pwd === process.env.BASIC_AUTH_PASS
+    ) {
+      return NextResponse.next()
+    }
+  }
+
+  return new NextResponse('Auth required', {
+    status: 401,
+    headers: { 'WWW-Authenticate': 'Basic realm="Secure Area"' },
+  })
+}
