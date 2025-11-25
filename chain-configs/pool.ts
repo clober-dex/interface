a82626d375285f3513fb1a2afda46f98fbae3d07
graykode <nlkey2022@gmail.com>
2025-11-25 20:57:10 +0900

    feat: add new pool key to the pool configuration

--- chain-configs/pool.ts
+++ chain-configs/pool.ts
@@ -8,4 +8,8 @@ export const WHITELISTED_POOL_KEY_AND_WRAPPED_CURRENCIES: {
     poolKey:
       '0xbe1bdd34a1c3fdc238c581aa52ffd5f5ebb61e229ee65197f00679df7d51beea',
   },
+  {
+    poolKey:
+      '0x6cfff26d468c939f54a469dbb0c49ed2e9ffc00a050812c6dfdc02d20ceb2b3a',
+  },
 ]
